8453b9306ef1de8c32f14a6cb9d5a13ba50122a1
Eduardo Macedo <eduzemacedo@hotmail.com>
2020-07-24 14:17:14 -0300

    Added thumbnails on recyclerview and started using enums for primary playlists

--- app/src/main/java/me/edujtm/tuyo/data/model/PrimaryPlaylists.kt
+++ app/src/main/java/me/edujtm/tuyo/data/model/PrimaryPlaylists.kt
@@ -2,18 +2,16 @@ package me.edujtm.tuyo.data.model
 
 typealias PlaylistId = String
 
-data class PrimaryPlaylists(
+data class PrimaryPlaylistsIds(
     val likedVideos: PlaylistId,
     val history: PlaylistId,
     val favorites: PlaylistId,
     val watchLater: PlaylistId
-) {
+)
 
-    // TODO: see if I'm able to turn this into an enum with R8 optimization
-    companion object {
-        const val LIKED_VIDEOS = 0
-        const val WATCH_HISTORY = 1
-        const val FAVORITES = 2
-        const val WATCH_LATER = 3
-    }
+enum class PrimaryPlaylist {
+    LIKED_VIDEOS,
+    WATCH_HISTORY,
+    FAVORITES,
+    WATCH_LATER
 }
